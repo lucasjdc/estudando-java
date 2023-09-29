27fd8385eb168e9ff3a203e6f072d14c6f71da2e
Lucas Costa <lucasjpdc@gmail.com>
2023-09-29 11:59:09 -0300

    apaguei a lista de genero

--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 public class Filme {
     private String nome;
-    private ArrayList<String > genero  = new ArrayList<String>();
     private String direcao;
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
