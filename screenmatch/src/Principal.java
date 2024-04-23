2c9cad0982140fb1e56d4180ee5c8beab345df96
lucasjdc <lucasjpdc@gmail.com>
2024-04-23 19:29:18 -0300

    Lista de filmes com ArrayList

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -4,13 +4,14 @@ import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
+import java.util.ArrayList;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
         meuFilme.setNome("Rambo: Programado Para Matar");
         meuFilme.setAnoDeLancamento(1982);
         meuFilme.setDuracaoEmMinutos(97);
-
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
@@ -50,5 +51,20 @@ public class Principal {
         episodio.setTotalVisualizacoes(300);
 
         filtro.filtra(episodio);
+
+        var filmeDoPaulo = new Filme();
+        filmeDoPaulo.setNome("Dogville");
+        filmeDoPaulo.setAnoDeLancamento(2003);
+        filmeDoPaulo.setDuracaoEmMinutos(177);
+        filmeDoPaulo.avalia(10);
+
+        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+        listaDeFilmes.add(filmeDoPaulo);
+        listaDeFilmes.add(meuFilme);
+        listaDeFilmes.add(outroFilme);
+
+        System.out.println("Tamanho da lista: " + listaDeFilmes.size());
+        System.out.println("Primeiro filme: " + listaDeFilmes.get(0).getNome());
+        System.out.println(listaDeFilmes);
     }
 }
